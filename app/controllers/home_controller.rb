1eeec71683c6477f6c0c286125a301ff9d2d2e21
petek157 <petek157@Peter-Korugas-MacBook-Pro-1625.local>
2019-09-29 20:39:44 -0500

    commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,38 @@
 class HomeController < ApplicationController
 
-  layout 'front'
+  # layout 'front'
 
   def index
     @test = "Im Peter"
+    render layout: "index"
+  end
+
+  def explore
+    @title = "Lets Explore"
+    @subtitle = "These are things to do"
+    @message = "This will be the general welcome message. It should be about 5-8 lines long on a full screen and can be clip shorter on a mobile device. 'alihs'lhglkjnafdgm fg kjnfaj skdnf sn kj dfoigIOPqj jkdf gj wd kdfj hdwkjfhv jlwhjhe jfhq jhe fgjh wdfjgh dwjfhg j,wdhf gwhdf g.hwdf.gh feg;qheghqjfhv jhd vjqh dgh ejrhg jhfg jhdfgjqejrgquertiuqergiuqf gjh qdfgjh qehgjqh egjh qjge qjfgiuqeg qjegh qidf gjh qefghqihe gjhq egjh qejg jqfgj qfegh qejhr fjh dfjh qjerh tjqh ejrth qjerht jqr efjhqrehrf."
+    render layout: "front"
+  end
+
+  def shop
+    @title = "Local Businesses"
+    @subtitle = "Find the products and service you need."
+    @message = "This will be the general welcome message. It should be about 5-8 lines long on a full screen and can be clip shorter on a mobile device. 'alihs'lhglkjnafdgm fg kjnfaj skdnf sn kj dfoigIOPqj jkdf gj wd kdfj hdwkjfhv jlwhjhe jfhq jhe fgjh wdfjgh dwjfhg j,wdhf gwhdf g.hwdf.gh feg;qheghqjfhv jhd vjqh dgh ejrhg jhfg jhdfgjqejrgquertiuqergiuqf gjh qdfgjh qehgjqh egjh qjge qjfgiuqeg qjegh qidf gjh qefghqihe gjhq egjh qejg jqfgj qfegh qejhr fjh dfjh qjerh tjqh ejrth qjerht jqr efjhqrehrf."
+    render layout: "front"
+  end
+
+  def stay
+    @test = "Lets Sleep"
+    render layout: "front"
+  end
+
+  def maps
+    @test = "Where am I?"
+    render layout: "front"
+  end
+
+  def conditions
+    @test = "What are the trails like?"
+    render layout: "front"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get 'home/index'
+  get 'explore', to: 'home#explore'
+  get 'shop', to: 'home#shop'
+  get 'stay', to: 'home#stay'
+  get 'maps', to: 'home#maps'
+  get 'conditions', to: 'home#conditions'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
